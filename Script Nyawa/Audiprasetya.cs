b45541660ff9fe8a8ebab5454adf67a596b6b31a
audiprasetya <46815104+audiprasetya@users.noreply.github.com>
2020-05-11 14:34:59 +0700

    Scripts

---
+++ Script Nyawa/Audiprasetya.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audiprasetya : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	GameObject playerInstance;
+
+	public int numLives = 4;
+
+	float respawnTimer;
+
+	// Use this for initialization
+	void Start () {
+		SpawnPlayer();
+	}
+
+	void SpawnPlayer() {
+		numLives--;
+		respawnTimer = 1;
+		playerInstance = (GameObject)Instantiate(playerPrefab, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(playerInstance == null && numLives > 0) {
+			respawnTimer -= Time.deltaTime;
+
+			if(respawnTimer <= 0) {
+				SpawnPlayer();
+			}
+		}
+	}
+
+	void OnGUI() {
+		
+		if(numLives > 0 || playerInstance!= null) {
+			GUI.Button( new Rect(0, 0, 100, 50), "Nyawa: " + numLives);
+		}
+		else {
+			 GUI.Label(new Rect(650, 300, 100, 20), "Coba lagi dong");
+
+		}
+	}
+}
---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+         Debug.Log("Memulai Permainan");
+ 
+    }
+
+  public void QuitGame () 
+  {
+ Application.Quit ();
+ Debug.Log("Sampai jumpa, nanti main lagi ya");
+ }
+}
